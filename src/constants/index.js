1774446592718fe6df200f8b444262781511f72f
fvidalarcon <fernando.v.alarcon@gmail.com>
2020-10-10 18:07:15 -0300

    Components

---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+//url del api 
+
+const base_url = 'http://cne.cloudapi.junar.com/api/v2/datastreams/VENTA-NACIO-POR-PRODU-Y/data.json/?auth_key=';
+const api_key = 'VBgOcFIHksODkvxUCeG6EAEUpuS4tMbvnOHBfE59';
+
+
+export const getApiUrl = query => `${ base_url }`
